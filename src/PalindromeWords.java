214c5d81522e9adb10b5d511a4b8786f5d627adf
Safak Kerem Balci <kerembalci@gmail.com>
2022-07-15 10:58:31 +0300

    first commit

---
+++ src/PalindromeWords.java
@@ -0,0 +1,33 @@
+
+public class PalindromeWords {
+
+    public static void main(String[] args) {
+
+        System.out.println(isPalindrome2("kayak"));
+
+    }
+
+    static boolean isPalindrome(String str){
+
+        int i = 0;
+        int j = str.length() - 1;
+
+        while (i < j){
+            if (str.charAt(i) != str.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    static boolean isPalindrome2(String str){
+
+        StringBuilder reverse = new StringBuilder();
+        for (int i = str.length() - 1; i >= 0; i--){
+            reverse.append(str.charAt(i));
+        }
+        return str.equals(reverse.toString());
+    }
+}
